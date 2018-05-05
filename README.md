b017aae514457959bab85f1733a9f817f03839ac
Stephen <stephen.ml@hotmail.com>
2018-05-05 16:08:24 +0800

    改用Oimojs实现物理效果

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # `Threejs` 开发微信小游戏
 
-本项目是一个使用 `Threejs` 开发微信小游戏的示例， 使用了 `Physijs` 库用作物理效果
+本项目是一个使用 `Threejs` 开发微信小游戏的示例， 使用了 ~~`Physijs`~~  `Oimojs` 库用作物理效果
 
 ---
 
@@ -16,20 +16,37 @@
 
 ---
 
-`Physijs` : https://github.com/chandlerprall/Physijs
+~~`Physijs` : https://github.com/chandlerprall/Physijs~~
 
-由于 `Physijs` 是基于 `worker` 进行模拟， 而微信小游戏中的 `worker` 并非原生，所以需要对 `physi.js` 、 `physijs_worker.js` 中的部分代码进行修改才能适配微信小游戏
+~~由于 `Physijs` 是基于 `worker` 进行模拟， 而微信小游戏中的 `worker` 并非原生，所以需要对 `physi.js` 、 `physijs_worker.js` 中的部分代码进行修改才能适配微信小游戏~~
 
 ---
 
-`Ammojs` : https://github.com/kripken/ammo.js
+~~`Ammojs` : https://github.com/kripken/ammo.js~~
 
-目前直接引入的是 `Physijs` 示例中的 `ammo.js`，有条件的可自行下载进行编译
+~~目前直接引入的是 `Physijs` 示例中的 `ammo.js`，有条件的可自行下载进行编译~~
+
+---
+
+`Oimojs` : https://github.com/lo-th/Oimo.js
+
+引入方式同 `Threejs`
 
 ---
 
 # 遇到的问题
 
-1. 使用 `Physijs` 示例中的 `ammo.js` 开发工具可以正常模拟，手机预览会报错，暂时还没有进行适配
+1. ~~使用 `Physijs` 示例中的 `ammo.js` 开发工具可以正常模拟，手机预览会报错，暂时还没有进行适配~~
 
-2. `ammo.js` 的体积太大，而微信小游戏的包大小限制为 `4M` , 占用了 `1/3` 的大小 （`Threejs` 的占用大小约为 `600k`）
+2. ~~`ammo.js` 的体积太大，而微信小游戏的包大小限制为 `4M` , 占用了 `1/3` 的大小 （`Threejs` 的占用大小约为 `600k`）~~
+
+3. 使用 `Oimojs` 手机预览时，如果有 `console.log( OimoObject )` 类似操作，可能会造成无法渲染，解决办法就是删除这行调试代码
+
+---
+
+# 更新日志
+
+### 2018年05月05日
+```
+> 移除 `Physijs` 和 `Ammojs` 改用 `Oimojs` 实现物理效果
+```
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'libs/threejs/three.min'
+import * as OIMO from 'libs/threejs/plugins/oimo.min'
 
 import 'libs/threejs/controls/OrbitControls'
 
@@ -8,9 +9,11 @@ const winWidth = window.innerWidth
 const winHeight = window.innerHeight
 const cameraAspect = winWidth / winHeight
 
+const length = 20
+
 const bastUrl = 'https://raw.githubusercontent.com/stephenml/wegame-threejs/master/model'
 
-export default class MainPhysics {
+export default class Main {
   constructor() {
     // 场景  
     this.scene = new THREE.Scene();
@@ -24,21 +27,33 @@ export default class MainPhysics {
 
     // 渲染器  
     this.renderer = new THREE.WebGLRenderer({ context: ctx, canvas: canvas })
-    this.renderer.shadowMapEnabled = true
+    this.renderer.shadowMap.enabled = true
     this.renderer.setSize(winWidth, winHeight)
     this.renderer.setClearColor(0xFFFFFF, 1)
     // 抗锯齿
     this.renderer.setPixelRatio(window.devicePixelRatio)
 
+    // 摄像机控制器
     this.controls = new THREE.OrbitControls(this.camera)
-
     this.controls.addEventListener('change', () => {
       this.renderer.render(this.scene, this.camera)
     })
 
+    // Oimo物理世界
+    this.world = new OIMO.World({ worldscale: 1 })
+
+    // 初始化
+    this.boxs = []
+    this.boxBodys = []
+    this.models = []
+    this.modelBodys = []
+
     this.start()
   }
   start() {
+    // 清除Oimo物理世界
+    this.world.clear()
+
     // 加载材质贴图
     let metal_texture = new THREE.TextureLoader().load("images/metal.jpg")
     let wood_texture = new THREE.TextureLoader().load("images/wood.jpg")
@@ -46,31 +61,69 @@ export default class MainPhysics {
 
     // 地面
     let ground_material = new THREE.MeshBasicMaterial({ map: metal_texture })
-    this.ground = new THREE.Mesh(new THREE.BoxGeometry(30, 1, 30), ground_material, 0)
+    this.ground = new THREE.Mesh(new THREE.BoxGeometry(30, 1, 30), ground_material)
     this.ground.receiveShadow = true
+    this.ground.castShadow = true
     this.scene.add(this.ground)
 
-    // 盒子
-    let box_meterial = new THREE.MeshLambertMaterial({ map: wood_texture })
-    this.box = new THREE.Mesh(new THREE.BoxGeometry(4, 4, 4), box_meterial)
-    this.box.position.y = 15
-    this.box.collisions = 0
-    this.box.castShadow = true
-    this.scene.add(this.box)
+    // 物理地面
+    this.groundBody = this.world.add({
+      size: [30, 1, 30],
+      pos: [0, 0, 0],
+      name: 'groundBody'
+    })
 
     // 载入json模型
     new THREE.JSONLoader().load(`${bastUrl}/sphere.json`,
       (geometry, materials) => {
         geometry.center()
-        this.model_meterial = new THREE.MeshLambertMaterial({ map: cloth_texture })
+        // 盒子材质
+        let box_meterial = new THREE.MeshLambertMaterial({ map: wood_texture })
+        // 模型材质
+        let model_meterial = new THREE.MeshLambertMaterial({ map: cloth_texture })
 
-        this.model = new THREE.Mesh(geometry, this.model_meterial)
-        this.model.scale.set(2, 2, 2)
-        this.model.position.y = 10
-        this.model.collisions = 0
-        this.model.castShadow = true
-        this.scene.add(this.model)
+        // 载入模型后再创建盒子
+        // 创建盒子和模型
+        for (let i = 0; i < length; i++) {
+          // 创建盒子
+          let box = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), box_meterial)
+          box.position.y = 50 + i * 2
+          box.receiveShadow = true
+          box.castShadow = true
+          this.scene.add(box)
+          this.boxs.push(box)
 
+          // 创建物理盒子
+          let boxBody = this.world.add({
+            type: 'box',
+            move: true,
+            size: [3, 3, 3],
+            pos: [0, 50 + i * 2, 0],
+            name: `boxBody${i}`
+          })
+          this.boxBodys.push(boxBody)
+
+          // 创建模型
+          let model = new THREE.Mesh(geometry, model_meterial)
+          model.scale.set(2, 2, 2)
+          model.position.y = 50 + i * 2
+          model.receiveShadow = true
+          model.castShadow = true
+          this.scene.add(model)
+          this.models.push(model)
+
+          // 创建物理模型
+          let modelBody = this.world.add({
+            type: 'sphere',
+            move: true,
+            size: [2],
+            pos: [0, 50 + i * 2, 0],
+            name: `modelBody${i}`
+          })
+          this.modelBodys.push(modelBody)
+        }
+
+        // 开启主线程
         this.loop()
       },
       // 进度条 小游戏内无效
@@ -85,8 +138,24 @@ export default class MainPhysics {
 
   }
   loop() {
-    this.box.rotation.z += .1
-    this.model.rotation.y += .1
+    // 更新物理世界
+    this.world.step()
+
+    // 复制物理世界的位置到Threejs的网格上
+    for (let i = 0; i < length; i++) {
+      this.boxs[i].position.copy(this.boxBodys[i].getPosition())
+      this.boxs[i].quaternion.copy(this.boxBodys[i].getQuaternion())
+      
+      this.models[i].position.copy(this.modelBodys[i].getPosition())
+      this.models[i].quaternion.copy(this.modelBodys[i].getQuaternion())
+    }
+
+    // 碰撞检测
+    // if (this.world.checkContact('boxBody', 'modelBody')) {
+    //   console.log('contact...')
+    // }
+
+    // 渲染
     this.renderer.render(this.scene, this.camera)
     window.requestAnimationFrame(this.loop.bind(this), canvas)
   }
